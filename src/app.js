bb83308890e93e2379ba04b00775e5a503778222
Ankit Kumar <98731902+ankit-kr27@users.noreply.github.com>
2024-01-24 23:39:46 +0530

    user and video models

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ import cookieParser from 'cookie-parser'
 
 const app = express();
 
+// *** MIDDLEWARES ***
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-// *** MIDDLEWARES ***
 app.use(express.json({limit:"16kb"}))   // this middleware is used to handle requests with json data format and we've set the limit to 16kb
 app.use(express.urlencoded({extended: true, limit: "16kb"}))    // extended is not a necessity
 // this middleware is used to handle requests different types of url encoding 
